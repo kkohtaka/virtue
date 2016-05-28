08465f699677910cf1c467ab3409c54ac0013b00
Kazumasa Kohtaka <kkohtaka@gmail.com>
2016-05-29 00:44:08 +0900

    Add configurations for VPC and key-pair

--- virtue.tf
+++ virtue.tf
@@ -29,17 +29,67 @@ provider "aws" {
   region = "${var.aws_region}"
 }
 
-resource "aws_iam_role" "virtue_role" {
-  name = "virtue-role"
-  assume_role_policy = "${file("iam-policies/sts-role.json")}"
+resource "aws_iam_role" "ecs_role" {
+  name = "ecs-role"
+  assume_role_policy = "${file("iam-policies/ecs-role.json")}"
 }
 
-resource "aws_iam_role_policy" "virtue_role_policy" {
-  name = "virtue-role-policy"
-  role = "${aws_iam_role.virtue_role.id}"
+resource "aws_iam_role_policy" "ecs_role_policy" {
+  name = "ecs-role-policy"
+  role = "${aws_iam_role.ecs_role.id}"
   policy = "${file("iam-policies/ecs-policy.json")}"
-  depends_on = ["aws_iam_role.virtue_role"]
+  depends_on = ["aws_iam_role.ecs_role"]
 }
+
+resource "aws_key_pair" "virtue_key_pair" {
+  key_name = "virtue-key-pair"
+  public_key = "${file(".ssh/id_rsa.pub")}"
+}
+
+resource "aws_vpc" "virtue_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags {
+    Name = "virtue-vpc"
+  }
+}
+
+resource "aws_security_group" "web" {
+  name = "web"
+  description = "Allow inbound HTTP and HTTPS traffic"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  vpc_id = "${aws_vpc.virtue_vpc.id}"
+
+  tags {
+    Name = "web"
+  }
+}
+
+
 
 resource "aws_elb" "virtue_elb" {
   name = "virtue-elb"
@@ -75,9 +125,9 @@ resource "aws_ecs_service" "prometheus_service" {
   cluster = "${aws_ecs_cluster.virtue_cluster.id}"
   task_definition = "${aws_ecs_task_definition.prometheus_task.arn}"
   desired_count = 3
-  iam_role = "${aws_iam_role.virtue_role.arn}"
+  iam_role = "${aws_iam_role.ecs_role.arn}"
   depends_on = [
-    "aws_iam_role.virtue_role"
+    "aws_iam_role.ecs_role"
   ]
 
   load_balancer {
